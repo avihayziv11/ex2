96426b9af691308e3615f9560f1df63f9e0acbc8
Avihay Ziv <avihayziv11@gmail.com>
2025-11-21 17:53:09 +0200

    ex2 task

--- ex2.c
+++ ex2.c
@@ -1,7 +1,7 @@
 /******************
-Name:
-ID:
-Assignment:
+Name: Avihay Ziv
+ID: 216022558
+Assignment: ex2
 *******************/
 
 #include <stdio.h>
@@ -9,20 +9,215 @@ Assignment:
 int main()
 {
     // MAIN MENU LOOP
+    
+    // Game Choice
+    int gameChoice; 
+    
+    // Ducky Unity Game variables
+    int decimalNum, goldenCorns, tempNum;  
+    // The Memory Game variables
+    int ducksInline, flag;
+    unsigned long long quakOrSH, tempQuak;
+    // Professor Pat's Power Calculation
+    int baseNum, exponentNum, powerNum;
+    // The Duck Parade
+    int drawnDucks, tempDucks, runDucks;
+    // The Mystery of the Repeated Digits
+    int secretCode, countNum, backwardsCode, tempCode, checkNum, numTimes;
 
-    // TASK 1: Ducky's Unity Game
+    do {
+        printf("Welcome to our games, please choose an option:\n");
+        printf("1. Ducky's Unity Game\n");
+        printf("2. The Memory Game\n");
+        printf("3. Professor Pat's Power Calculation\n");
+        printf("4. The Duck Parade\n");
+        printf("5. The Mystery of the Repeated Digits\n");
+        printf("6. Good Night Ducks\n");
 
-    // TASK 2: The Memory Game
+        scanf("%d", &gameChoice);
 
-    // TASK 3: Professor Pat's Power Calculation
+        switch(gameChoice) {
+            case 1: {
+                printf("please enter a positive number:\n");
+                do {
+                    scanf("%d", &decimalNum);
+                    if(decimalNum > 0) {
+                        goldenCorns = 0;
+                        tempNum = decimalNum;
+                        while(tempNum > 0) {
+                            if(tempNum % 2 != 0) 
+                                goldenCorns++;
+                            tempNum /= 2;
+                        }
+                        printf("Ducky earns %d corns\n", goldenCorns);
+                    }
+                    else {
+                        printf("Invalid number, please try again\n");
+                    }
+                } while(decimalNum <= 0); 
+                break;
+            }
+            
+            case 2: {
+                quakOrSH = 0; 
+                printf("please enter the number of ducks:\n");
+                do {
+                    scanf("%d", &ducksInline);
+                    
+                    if (ducksInline > 0) {
+                        printf("you entered %d ducks\n", ducksInline);
+                        for(int i = 0; i < ducksInline; i++) { 
+                            printf("duck %d do QUAK? 1 for yes, 0 for no\n", i+1);
+                            do{
+                                scanf("%d", &flag);
+                                if(flag!=0&&flag!=1)
+                                    printf("invalid number please try again\n");
+                            }while(flag!=0&&flag!=1);
+                            
+                            
+                            if(flag == 1)
+                                quakOrSH = quakOrSH | (1ULL << i);       
+                        }
+                        
+                        for(int i=0; i < ducksInline; i++){
+                            if(quakOrSH & (1ULL << i))
+                                printf("duck number %d do Quak\n", i+1);
+                            else
+                                printf("duck number %d do Sh...\n", i+1);
+                        }
+                        
+                    } 
+                    else {
+                        printf("Invalid number, please try again\n");
+                    }
 
-    // TASK 4: The Duck Parade
+                } while(ducksInline <= 0);
+                break;
+            }
+            
+            case 3: {
+                powerNum=1;
+                printf("please enter the number\n");
+                do{
+                    scanf(" %d", &baseNum);
+                    if (baseNum<=0)
+                        printf("invalid number, please try again\n");
+                }while(baseNum<=0);
+                printf("please enter the exponent\n");
+                do{
+                    scanf(" %d", &exponentNum);
+                    if (exponentNum<=0)
+                        printf("invalid number, please try again\n");
+                }while(exponentNum<=0);
+                for(int i=0; i<exponentNum; i++)
+                    powerNum*=baseNum;
+                printf(" %d\n", powerNum);
+                break;
+            }
+            
+            case 4: {
+                printf("please enter number of ducks:\n");
+                do{
+                    scanf(" %d", &drawnDucks);
+                    if(drawnDucks<=0)
+                        printf("invalid number, please try again\n");
+                    
+                }while(drawnDucks<=0);
+                runDucks = drawnDucks;
+                for(int j=0; j<((runDucks/10)+1); j++){
+                    if(drawnDucks/10>0){
+                        tempDucks=10;
+                        for(int i=0; i<tempDucks; i++){
+                            printf("   _    ");
+                            if(i+1==tempDucks)
+                                printf("\n");
+                        }
+                        for(int i=0; i<tempDucks; i++){
+                            printf("__(0)>  ");
+                            if(i+1==tempDucks)
+                                printf("\n");
+                        }
+                        for(int i=0; i<tempDucks; i++){
+                            printf("\\___)   ");
+                            if(i+1==tempDucks)
+                                printf("\n");
+                        }
+                        drawnDucks-=10;
+                    }
+                    else{
+                        tempDucks=drawnDucks;
+                        for(int i=0; i<tempDucks; i++){
+                            printf("   _    ");
+                            if(i+1==tempDucks)
+                                printf("\n");
+                        }
+                        for(int i=0; i<tempDucks; i++){
+                            printf("__(0)>  ");
+                            if(i+1==tempDucks)
+                                printf("\n");
+                        }
+                        for(int i=0; i<tempDucks; i++){
+                            printf("\\___)   ");
+                            if(i+1==tempDucks)
+                                printf("\n");
+                        }
 
-    // TASK 5: The Mystery of the Repeated Digits
+                    }
 
-    // TASK 6: EXIT
+                }
+                break;
+            }
+            case 5: {
+                printf("please enter number:\n");
+                do {
+                    scanf("%d", &secretCode);
+                    if(secretCode <= 0)
+                        printf("Invalid number, please try again\n");
+                } while(secretCode <= 0);
 
-    // dont forget through all the necessary parts to validate invalid input.
+                tempCode = secretCode;
+                int countNum = 0;
+                while(tempCode > 0) {
+                    countNum++;
+                    tempCode = tempCode / 10;
+                }
+
+                while(secretCode > 0) {
+                    checkNum = secretCode % 10;
+                    secretCode = secretCode / 10;
+                    
+                    tempCode = secretCode;
+                    numTimes = 0; 
+
+                    while(tempCode > 0) {
+                        if(tempCode % 10 == checkNum) {
+                            numTimes++;
+                        }
+                        else {
+                            numTimes = numTimes;
+                        }
+                        tempCode = tempCode / 10;
+                    }
+
+                    if(numTimes > 0) {
+                        printf("%d appears more than once!\n", checkNum);
+                    }
+                }
+                break;
+            }
+            
+            case 6: {
+                printf("Good night! See you at the pond tomorrow.\n");
+                break;
+            } 
+            
+            default: {
+                printf("Invalid option, please try again\n");
+                break;
+            }
+        }
+        
+    } while(gameChoice != 6);
 
     return 0;
 }
